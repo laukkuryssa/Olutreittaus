703b68ac168d5a5253a6aaa9b038b5b027354278
0141456267 <timo.hautamaki@hotmail.com>
2016-02-06 18:17:25 +0200

    paivitys

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -7,11 +7,12 @@ end
 def new
 	@rating = Rating.new
 	@beers = Beer.all
-	@users = User.all
+	@users = User.all 
 end
 
 def create
     rating = Rating.create params.require(:rating).permit(:score, :beer_id, :user_id)
+    current_user.ratings << rating
     session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
     redirect_to ratings_path
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
 
     has_many :ratings
 
+    def count
+	return ratings.count
+    end
 end
